0116cd1a331d3a8fff1ecfd8799e5f84ddf8a04b
apitz <adrien.pitz@student.ulg.ac.be>
2018-03-24 23:31:55 +0100

    Add Google Image Compatibility

--- Spotify Local Tagger/SpotifySong.cs
+++ Spotify Local Tagger/SpotifySong.cs
@@ -58,12 +58,29 @@ namespace Spotify_Local_Tagger
                     if(imageBytes == null || imageBytes.Count() == 0)
                     {
                         //TODO : Set default album cover
+
+                        Stream albumPicStream = new GoogleImageAlbum().getAlbumImage(track.Track.Album.Name, null); // TODO : Remove empty string
+
+                        if (albumPicStream == null)
+                        {
+                            Console.WriteLine("Coucou !");
+                            Assembly _assembly = Assembly.GetExecutingAssembly();
+                            albumPicStream = _assembly.GetManifestResourceStream("Spotify_Local_Tagger.Resources.localTaggerDefaultAlbum.bmp");
+                        }
+
+                        System.Drawing.Image thepic = new Bitmap(albumPicStream);
+                        var ms = new MemoryStream();
+                        thepic.Save(ms, thepic.RawFormat);
+                        imageBytes = ms.ToArray();
+
+                        /**
                         Assembly _assembly = Assembly.GetExecutingAssembly();
                         Stream albumPicStream = _assembly.GetManifestResourceStream("Spotify_Local_Tagger.Resources.localTaggerDefaultAlbum.bmp");
                         System.Drawing.Image thepic = new Bitmap(albumPicStream);
                         var ms = new MemoryStream();
                         thepic.Save(ms, thepic.RawFormat);
                         imageBytes = ms.ToArray();
+                        **/
                     }
 
                 }catch(Exception e)
@@ -73,12 +90,29 @@ namespace Spotify_Local_Tagger
             }else
             {
                 //TODO : Set default album cover
+
+                Stream albumPicStream = new GoogleImageAlbum().getAlbumImage(track.Track.Album.Name, null); // TODO : Remove empty string
+
+                if (albumPicStream == null)
+                {
+                    Console.WriteLine("Coucou !");
+                    Assembly _assembly = Assembly.GetExecutingAssembly();
+                    albumPicStream = _assembly.GetManifestResourceStream("Spotify_Local_Tagger.Resources.localTaggerDefaultAlbum.bmp");
+                }
+
+                System.Drawing.Image thepic = new Bitmap(albumPicStream);
+                var ms = new MemoryStream();
+                thepic.Save(ms, thepic.RawFormat);
+                imageBytes = ms.ToArray();
+
+                /**
                 Assembly _assembly = Assembly.GetExecutingAssembly();
                 Stream albumPicStream = _assembly.GetManifestResourceStream("Spotify_Local_Tagger.Resources.localTaggerDefaultAlbum.bmp");
                 System.Drawing.Image thepic = new Bitmap(albumPicStream);
                 var ms = new MemoryStream();
                 thepic.Save(ms, thepic.RawFormat);
                 imageBytes = ms.ToArray();
+                **/
             }
         }
 
@@ -190,6 +224,7 @@ namespace Spotify_Local_Tagger
                 return (uint)fullAlbum.Tracks.Items.Count();
 
             return 0;
+    
         }
 
         public TagLib.Id3v2.AttachedPictureFrame getAlbumImage()
@@ -202,5 +237,142 @@ namespace Spotify_Local_Tagger
             return albumCoverPictFrame;
         }
 
+        private class GoogleImageAlbum
+        {
+
+            public GoogleImageAlbum()
+            {
+
+            }
+
+            public Stream getAlbumImage(string nameOfAlbum, string mainPerformer)
+            {
+                Stream albumImageStream = null;
+
+                string html = getHTML(nameOfAlbum, mainPerformer);
+                List<string> urls = getUrls(html);
+
+                byte[] cover = getImage(urls.ElementAt(0));
+
+                albumImageStream = new MemoryStream(cover);
+
+                if(albumImageStream == null)
+                {
+                    Console.WriteLine("Nique ta mère");
+                }
+
+                return albumImageStream;
+            }
+
+            private static string getHTML(string nameOfAlbum, string mainPerformer)
+            {
+
+                string[] nameOfAlbumSplitted;
+                string[] mainPerformerSplitted;
+
+                var delimiter = ' ';  // Cut on spaces
+
+                // First, let's make the query
+                if (nameOfAlbum != null)
+                    nameOfAlbumSplitted = nameOfAlbum.Split(delimiter);
+                else
+                    nameOfAlbumSplitted = new string[] { };
+
+                if (mainPerformer != null)
+                    mainPerformerSplitted = mainPerformer.Split(delimiter);
+                else
+                    mainPerformerSplitted = new string[] { };
+
+                string query = "https://www.google.be/search?q=";
+
+                for (var i=0; i < nameOfAlbumSplitted.Length - 1; ++i)
+                {
+                    query += (nameOfAlbumSplitted[i] + "+");
+                }
+
+                if(nameOfAlbumSplitted.Length > 0 && mainPerformerSplitted.Length == 0)
+                {
+                    query += nameOfAlbumSplitted[nameOfAlbumSplitted.Length - 1]; // Query terminated
+                }
+                else if(nameOfAlbumSplitted.Length > 0)
+                {
+                    query += (nameOfAlbumSplitted[nameOfAlbumSplitted.Length - 1] + "+"); //Query continues for the artist
+                }
+
+                for (var i=0; i < mainPerformerSplitted.Length - 1; ++i)
+                {
+                    query += (mainPerformerSplitted[i] + "+");
+                }
+
+                if(mainPerformerSplitted.Length > 0)
+                {
+                    query += mainPerformerSplitted[mainPerformerSplitted.Length - 1];
+                }
+
+                query += "&tbm=isch"; // Specify that we want the google image page
+
+                var htmlData = "";
+
+                var request = (HttpWebRequest)WebRequest.Create(query);
+                request.Accept = "text/html, application/xhtml+xml, */*";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
+
+                var response = (HttpWebResponse)request.GetResponse();
+
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    if (dataStream == null)
+                        return "";
+                    using (var sr = new StreamReader(dataStream))
+                    {
+                        htmlData = sr.ReadToEnd();
+                    }
+                }
+
+                return htmlData;
+
+            }
+
+            private static List<string> getUrls(string html)
+            {
+                var urls = new List<string>();
+
+                int i = html.IndexOf("\"ou\"", StringComparison.Ordinal);
+
+                while (i >= 0)
+                {
+                    i = html.IndexOf("\"", i + 4, StringComparison.Ordinal);
+                    i++;
+                    int ndx2 = html.IndexOf("\"", i, StringComparison.Ordinal);
+                    string url = html.Substring(i, ndx2 - i);
+                    urls.Add(url);
+                    i = html.IndexOf("\"ou\"", ndx2, StringComparison.Ordinal);
+                    break; // TODO : Remove if we want more thant one pic
+                }
+
+                return urls;
+            }
+
+            private static Byte[] getImage(string url)
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                var response = (HttpWebResponse)request.GetResponse();
+
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    if (dataStream == null)
+                        return null;
+                    using (var sr = new BinaryReader(dataStream))
+                    {
+                        byte[] bytes = sr.ReadBytes(100000000);
+
+                        return bytes;
+                    }
+                }
+            }
+
+        }
+
     }
+
 }
